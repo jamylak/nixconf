e184a3def804343932cd6c8de49a9f5a10e9ae98
jamylak <jamylak@gmail.com>
2026-01-20 17:42:10 +1100

    modesetting

--- hosts/vmware.nix
+++ hosts/vmware.nix
@@ -10,7 +10,7 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.videoDrivers = [ "vmware" ];
+  services.xserver.videoDrivers = [ "modesetting" ];
 
   users.users.james.initialPassword = "changeme";
 
