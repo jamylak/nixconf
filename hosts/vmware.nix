c67497b3eec3473b0f94416b929d25f9721c0dd3
jamylak <jamylak@gmail.com>
2026-01-20 17:03:22 +1100

    initial vmware settings

--- hosts/vmware.nix
+++ hosts/vmware.nix
@@ -1,4 +1,8 @@
 { ... }: {
+  imports = [
+    ./vmware-hardware.nix
+  ];
+
   networking.hostName = "vmware";
 
   virtualisation.vmware.guest.enable = true;
@@ -7,4 +11,11 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.videoDrivers = [ "vmware" ];
+
+  users.users.james.initialPassword = "changeme";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.networkmanager.enable = true;
 }
