ff16265cfa983f59a73ae1341ece0f70e84d8397
jamylak <jamylak@gmail.com>
2026-01-20 23:38:45 +1100

    fix old names

--- hosts/vmware.nix
+++ hosts/vmware.nix
@@ -20,8 +20,8 @@
   };
 
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   services.xserver.videoDrivers = [ "modesetting" ];
 
   users.users.james.initialPassword = "changeme";
