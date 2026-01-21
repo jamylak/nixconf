312a9fe2dbbf7ec977039d2bf0785a4d5b4893ce
jamylak <jamylak@gmail.com>
2026-01-21 17:26:06 +1100

    disable toggle application view in gnome

--- hosts/nixos-common.nix
+++ hosts/nixos-common.nix
@@ -57,6 +57,7 @@
         toggle-message-tray = [];
         show-clipboard = [];
         show-all-apps = [];
+        toggle-application-view = [];
         switch-to-application-1 = [];
         switch-to-application-2 = [];
         switch-to-application-3 = [];
