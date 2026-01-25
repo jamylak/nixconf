626654cdb41d45d371f110f1f31439e81a8d2d1b
jamylak <jamylak@gmail.com>
2026-01-25 16:15:47 +1100

    try fix `exec-once`

--- home.nix
+++ home.nix
@@ -116,8 +116,8 @@ in {
       ];
 
       exec-once = [
-        "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-        "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+        "dbus-update-activation-environment --systemd --all"
+        "systemctl --user import-environment --all"
       ];
 
       general = {
