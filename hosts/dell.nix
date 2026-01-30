caa1f3e32299a38d0ef9bd28e061db489654827a
jamylak <jamylak@gmail.com>
2026-01-30 16:03:03 +1100

    fix dell

--- hosts/dell.nix
+++ hosts/dell.nix
@@ -1,4 +1,8 @@
 { pkgs, ... }: {
+  imports = [
+    ./dell-hardware.nix
+  ];
+
   networking.hostName = "dell";
 
   services.xserver.enable = true;
@@ -7,8 +11,6 @@
   services.xserver.videoDrivers = [ "amdgpu" ];
 
   hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
-  hardware.opengl.driSupport32Bit = true;
   hardware.opengl.extraPackages = [
     pkgs.amdvlk
   ];
