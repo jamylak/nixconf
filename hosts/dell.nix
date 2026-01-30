1c1071d49f1100ec755ce2ac4716f1c5c3ec5683
jamylak <jamylak@gmail.com>
2026-01-30 16:06:53 +1100

    dell boot loader

--- hosts/dell.nix
+++ hosts/dell.nix
@@ -10,6 +10,9 @@
   services.desktopManager.plasma6.enable = true;
   services.xserver.videoDrivers = [ "amdgpu" ];
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   hardware.opengl.enable = true;
   hardware.opengl.extraPackages = [
     pkgs.amdvlk
