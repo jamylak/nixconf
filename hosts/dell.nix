301e94053fa235db8f8ddc6f8c4bcc5426737fce
jamylak <jamylak@gmail.com>
2026-01-30 16:07:02 +1100

    remove deprecated packages

--- hosts/dell.nix
+++ hosts/dell.nix
@@ -14,10 +14,4 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   hardware.opengl.enable = true;
-  hardware.opengl.extraPackages = [
-    pkgs.amdvlk
-  ];
-  hardware.opengl.extraPackages32 = [
-    pkgs.driversi686Linux.amdvlk
-  ];
 }
